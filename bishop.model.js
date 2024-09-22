3e84c3d505cf40838fdafa5a3c011330b873db29
juliusbretto <162112719+juliusbretto@users.noreply.github.com>
2024-09-22 15:26:44 +0200

    Delete bishop.model.js

--- bishop.model.js
+++
@@ -1,9 +0,0 @@
-class Bishop {
-  static move(x1, y1, x2, y2) {
-    if (Math.abs(x2 - x1) === Math.abs(y2 - y1) && !(x2 === x1 && y2 === y1)) {
-      return true;
-    }
-    return false;
-  }
-}
-export default Bishop;
