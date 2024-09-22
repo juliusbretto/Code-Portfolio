a199bdd6149ad28b0176aea7570d32bb4f51946f
juliusbretto <162112719+juliusbretto@users.noreply.github.com>
2024-09-22 15:29:00 +0200

    Delete rook.model.js

--- rook.model.js
+++
@@ -1,9 +0,0 @@
-class Rook {
-  static move(x1, y1, x2, y2) {
-    if ((x2 === x1 || y2 === y1) && !(x2 === x1 && y2 === y1)) {
-      return true;
-    }
-    return false;
-  }
-}
-export default Rook;
